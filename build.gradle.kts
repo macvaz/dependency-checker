ac341f7a485f96d6922c4733db1e8e509d40c8dc
Miguel Angel Cañas Vaz <macvaz82@gmail.com>
2022-08-18 10:57:41 +0200

    Final doc

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.kafka:spring-kafka")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("org.liquibase:liquibase-core")
 
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.springframework.boot:spring-boot-devtools")
