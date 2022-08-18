c86132a8125f00c858f9b7e7afb9edede45c882c
Miguel Angel Cañas Vaz <macvaz82@gmail.com>
2022-08-18 11:06:01 +0200

    Disabled liquibase

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	implementation("org.liquibase:liquibase-core")
+	//implementation("org.liquibase:liquibase-core")
 
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.springframework.boot:spring-boot-devtools")
