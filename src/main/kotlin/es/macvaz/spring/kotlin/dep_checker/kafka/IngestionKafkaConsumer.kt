3aede566b5471b4553bee34fc20327532a3f60c0
Miguel Angel Cañas Vaz <macvaz82@gmail.com>
2022-08-19 11:02:55 +0200

    Working kafka integration based on simple string messages

--- src/main/kotlin/es/macvaz/spring/kotlin/dep_checker/kafka/IngestionKafkaConsumer.kt
+++ src/main/kotlin/es/macvaz/spring/kotlin/dep_checker/kafka/IngestionKafkaConsumer.kt
@@ -3,7 +3,6 @@ package es.macvaz.spring.kotlin.dep_checker.kafka
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,8 +10,7 @@ class IngestionKafkaConsumer {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @KafkaListener(topics = ["\${kafka.topics.ingestion.name}"], groupId = "\${kafka.topics.ingestion.group}")
-    fun listenGroup(consumerRecord: ConsumerRecord<Any, Any>, ack: Acknowledgment) {
+    fun listenGroup(consumerRecord: ConsumerRecord<Any, Any>) {
         logger.info("Message received {}", consumerRecord)
-        ack.acknowledge()
     }
 }
